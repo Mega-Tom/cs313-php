147d390d920145ffdaa61196e007a9f397327365
Mega-Tom <thomas@peckfam.com>
2018-09-29 22:43:02 -0600

    homepage

---
+++ web/assignments.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Thomas Peck</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1 class="glow"> Thomas Peck </h1>
+
+    <section id="main">
+        <h2>Comming soon!</h2>
+                <svg width="200" height="200" class="center">
+            <circle cx="100" cy="100" r="90" stroke="black" stroke-width="4" fill="white" />
+            <line x1="100" y1="100" x2="160" y2="150" style="stroke:rgb(0,0,0);stroke-width:6" />
+            <line x1="100" y1="100" x2="100" y2="50" style="stroke:rgb(0,0,0);stroke-width:6" />
+        </svg>
+    </section>
+    
+</body>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,16 +1,39 @@
 <html>
 <head>
-    <title>Main page</title>
+    <title>Thomas Peck</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <a href="/hello.html">hello</a>
+    <h1 class="glow"> Thomas Peck </h1>
 
-    <?php
-
-      echo("<p>this is from the php!</p>");
-
-    ?>
-
+    <section id="main">
+        <h2>Hello World!</h2>
+        <p>
+        I am Thomas Peck, a computer science student at BYU-Idaho. 
+        I do dancing, singing and math.
+        I also enjoy board games.
+        </p>
+        <table class="lights center">
+            <tbody>
+            <?php 
+                for($i = 0; $i < 4; $i++){
+                    echo "<tr>";
+                    for($j = 0; $j < 4; $j++)
+                    {
+                        echo "<td><input type='checkbox' id='check$i$j'/>";
+                        echo "<label for='check$i$j' /></td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+            </tbody>
+        </table>
+        <h2>Other Stuff</h2>
+        <p>
+        <a href="assignments.php">My assignments for Web engenering II</a>.
+        </p>
+    </section>
+    
 </body>
 </html>
