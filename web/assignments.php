1a8024e43a8e29ee16172b981951fc209ecb986f
Mega-Tom <thomas@peckfam.com>
2018-10-06 23:04:48 -0600

    link added

--- web/assignments.php
+++ web/assignments.php
@@ -8,12 +8,7 @@
     <h1 class="glow"> Thomas Peck </h1>
 
     <section id="main">
-        <h2>Comming soon!</h2>
-                <svg width="200" height="200" class="center">
-            <circle cx="100" cy="100" r="90" stroke="black" stroke-width="4" fill="white" />
-            <line x1="100" y1="100" x2="160" y2="150" style="stroke:rgb(0,0,0);stroke-width:6" />
-            <line x1="100" y1="100" x2="100" y2="50" style="stroke:rgb(0,0,0);stroke-width:6" />
-        </svg>
+        <a href="store">My store</a>
     </section>
     
 </body>
