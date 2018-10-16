d0f4ca930ab61d966c46251a1f53f3c1044a858b
Mega-Tom <thomas@peckfam.com>
2018-10-16 16:12:58 -0600

    TA 5.2

---
+++ web/TeamActivity/search.php
@@ -0,0 +1,24 @@
+<?php
+    require "dbconnect.php";
+?>
+
+<html>
+<head>
+    <title>Search</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+
+    <h1 class="glow"> Search Scripture Resources </h1>
+
+    <section id="main">
+    
+        <form action="scripture.php" method="GET">
+            <input type="text" name="book">
+            <input type="submit" value="search">
+        </form>
+
+     </section>
+    
+</body>
+</html>
