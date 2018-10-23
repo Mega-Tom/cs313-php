6a58741b5e4d10fe2af169652a164290878b2659
Mega-Tom <thomas@peckfam.com>
2018-10-23 16:45:05 -0600

    TA 6.2

--- web/TeamActivity/insertscripture.php
+++ web/TeamActivity/insertscripture.php
@@ -10,10 +10,10 @@
         $q = 'INSERT INTO Scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content)';
         
         $statment = $db->prepare($q);
-        $statment->bindValues(":book", $book, PDO::PARAM_STR);
-        $statment->bindValues(":chapter", $chapter, PDO::PARAM_INT);
-        $statment->bindValues(":verse", $verse, PDO::PARAM_INT);
-        $statment->bindValues(":content", $content, PDO::PARAM_STR);
+        $statment->bindValue(":book", $book, PDO::PARAM_STR);
+        $statment->bindValue(":chapter", $chapter, PDO::PARAM_INT);
+        $statment->bindValue(":verse", $verse, PDO::PARAM_INT);
+        $statment->bindValue(":content", $content, PDO::PARAM_STR);
         $statment->execute();
         header('Location: scripture.php');
 ?>
