0a1ad21b80122eb6d1f83610898f47ad881af86c
Mega-Tom <thomas@peckfam.com>
2018-10-27 21:58:59 -0600

    better error stuff

--- web/game/_stratego.php
+++ web/game/_stratego.php
@@ -38,6 +38,9 @@ class Piece {
         if($this->value == FLAG) return "F";
         return (string)$this->value;
     }
+    function __toString() {
+        return "{owner: ".$this->owner.", value: ".$this->value."}";
+    }
 }
 
 function valid_setup($positions) {
@@ -105,7 +108,7 @@ function do_move(&$board, $from, $to, $i) {
         $mover->owner != $current) {throw new Error("player $current cannot move from square $from (on move $i)");}
     if(
         $mover->value == BOMB ||
-        $mover->value == FLAG) {throw new Error("cannot move immobile piece (on move $i)");}
+        $mover->value == FLAG) {throw new Error("cannot move immobile piece $mover (on move $i)");}
     $board[$fy][$fx] = NULL;
     $targ = $board[$ty][$tx];
     if(! in_range($fx, $fy, $tx, $ty)) {throw new Error("cannot move from square $from to $to (on move $i)");}
