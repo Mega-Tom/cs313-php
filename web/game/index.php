ad823f2500f6f6df77bee28883efb1fb14a681a9
Mega-Tom <thomas@peckfam.com>
2018-11-01 17:42:35 -0600

    challenge list

--- web/game/index.php
+++ web/game/index.php
@@ -34,6 +34,30 @@
             echo "<li><a href='game.php?id=$game'> vs $opponent </a></li>";
         }
         echo "</ul>";
+        
+        $db = get_db();
+        $q = $db->prepare(
+            'SELECT p.playername as "opponent", r.id as "request" FROM Request r ' .
+                'JOIN player p ON challengerId = p.id ' .
+                'WHERE challengedId = :player ;'
+            );
+        $q->bindParam(":player", $_SESSION["user"]);
+        $q->execute();
+        
+        $results = $q->fetchAll();
+        
+        if(isset($results[0])) {
+            echo "<h2> Game requests: </h2>";
+            echo "<ul>";
+            foreach($results as $row){
+                $game = $row["request"];
+                $opponent = htmlspecialchars($row["opponent"]);
+                echo "<li> ";
+                echo "<a href='request.php?id=$request'> from $opponent </a>";
+                echo "</li>";
+            }
+            echo "</ul>";
+        }
     }else{
         echo "You are not logged in.";
     }
