77081f4db1c4f48a0e8960ae28a835a9745eb754
Mega-Tom <thomas@peckfam.com>
2018-11-03 17:26:45 -0600

    javascript css fixes

--- web/game/setup.js
+++ web/game/setup.js
@@ -1,22 +1,5 @@
-function swap(a, b) { //from https://stackoverflow.com/a/19033868/3990897
-    a = $(a); b = $(b);
-    var tmp = $('<span>').hide();
-    a.before(tmp);
-    b.before(a);
-    tmp.replaceWith(b);
-};
-
-function in_range(x1, y1, x2, y2) {
-    if(x1 == x2){
-        return y2 == y1 + 1 || y2 == y1 - 1;
-    }
-    else if(y1 == y2){
-        return x2 == x1 + 1 || x2 == x1 - 1;
-    }
-    return false;
-}
+$.ui.contains = $.contains; // hack nessisary for external library to work with newer jQuery
 
 $(function(){
-
-    $("table.board").swappable({items:"d.mobile"})
+    $("table.board").swappable({items:"d.mobile", cursorAt:{top:-5}}})
 })
